24ed39ba0c9f5c63d023e1f93ff764b425cd3f20
mustofaansori <ma979220@ohio.edu>
2025-01-22 19:54:15 -0500

    added hello git cout

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mustofa Ansori
+ *        Date: Jan 22, 2025
+ * Description: the last step
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
